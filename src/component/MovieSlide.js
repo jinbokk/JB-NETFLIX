8b5c6a5ead8294bdda92713b504ea49b916fc0be
JINBOK LEE <eyelash1024@naver.com>
2022-09-13 05:26:27 +0900

    change movie card num

--- src/component/MovieSlide.js
+++ src/component/MovieSlide.js
@@ -26,7 +26,7 @@ export default function MovieSlide({ movies }) {
       lazy={true}
       breakpoints={{
         0: {
-          slidesPerView: 3,
+          slidesPerView: 4,
           slidesPerGroup: 1,
         },
         450: {
